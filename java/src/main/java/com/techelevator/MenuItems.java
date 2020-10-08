65ef8359a07606c67d2bafb9168931b5af2cc0d8
Dennis Yoon <dhy6785@gmail.com>
2020-10-08 16:55:25 -0400

    Dennis Driver V1

--- java/src/main/java/com/techelevator/MenuItems.java
+++ java/src/main/java/com/techelevator/MenuItems.java
@@ -1,9 +1,17 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class MenuItems {
+import com.techelevator.view.Menu;
+
+public class MenuItems extends Menu {
 
 	//Private Members
 	private String snackID;
@@ -11,7 +19,7 @@ public class MenuItems {
 	private String snackPrice;
 	private String snackType;
 	private List<String> allSnacks = new ArrayList<String>();
-	
+
 	//Getters
 	public String getSnackID() {
 		return snackID;
@@ -28,6 +36,31 @@ public class MenuItems {
 	public List<String> getAllSnacks() {
 		return allSnacks;
 	}
-	
-	
+
+	public MenuItems(InputStream input, OutputStream output) {
+		super(input, output);
+	}
+
+	String inputPath = "vendingmachine.csv";
+	File inputFile = new File(inputPath);
+
+	public String snackDisplay(String snackID , String snackName, int snackPrice, String snackType) throws FileNotFoundException {
+		try(Scanner readCSV = new Scanner(inputFile)) {
+			while(readCSV.hasNextLine()) {
+				String currentLine = readCSV.nextLine();
+				String[] splitLine = currentLine.split("|");
+				for(String snack : allSnacks) {
+					if(splitLine[0].equals("A4")) {
+						
+					}
+
+
+				}
+
+
+			}
+			return null;
+		}
+
+	}
 }
--- java/src/main/java/com/techelevator/PurchaseLog.java
+++ java/src/main/java/com/techelevator/PurchaseLog.java
@@ -1,22 +1,39 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Date;
 
 public class PurchaseLog {
-
+	
 	String path = "Log.txt";
 	File audit = new File(path);
 	
-	public void snackEntry(String snackName, String snackID, String cost, String balance) {
+	public void snackEntry(String snackName, String snackID, String cost, String balance) throws FileNotFoundException {
 		Date date = java.util.Calendar.getInstance().getTime();
+		StringBuilder purchaseLog = new StringBuilder();
+		purchaseLog.append(date + " Purchased " + snackName + " " + snackID + " " + cost + " Balance" + balance);
+		PrintWriter writer = new PrintWriter(new FileOutputStream(audit, true));
+		writer.append(purchaseLog.toString());
 	}
 	
-	public void cashEntry() {
+	public void cashEntry() throws FileNotFoundException {
 		Date date = java.util.Calendar.getInstance().getTime();
+		StringBuilder purchaseLog = new StringBuilder();
+		// need variable
+//		purchaseLog.append(date + " Cash tendered");
+		PrintWriter writer = new PrintWriter(new FileOutputStream(audit, true));
+		writer.append(purchaseLog.toString());
 	}
 	
-	public void changeEntry() {
+	public void changeEntry() throws FileNotFoundException {
 		Date date = java.util.Calendar.getInstance().getTime();
+		StringBuilder purchaseLog = new StringBuilder();
+		// need variable
+//		purchaseLog.append(date + " Change dispensed");
+		PrintWriter writer = new PrintWriter(new FileOutputStream(audit, true));
+		writer.append(purchaseLog.toString());
 	}
 }
